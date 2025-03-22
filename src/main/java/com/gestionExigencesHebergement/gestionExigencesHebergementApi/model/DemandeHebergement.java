cb9e6fead6875874039580e7c5fc3e85a1dfbf0a
Mangassouba <hamallahmanga@gmail.com>
2025-03-22 15:22:06 +0000

    Ajout model demande hebergement

---
+++ src/main/java/com/gestionExigencesHebergement/gestionExigencesHebergementApi/model/DemandeHebergement.java
@@ -0,0 +1,30 @@
+package com.gestionExigencesHebergement.gestionExigencesHebergementApi.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DemandeHebergement {
+    @Id
+    @GeneratedValue
+
+    private long id;
+    private String libelle;
+    private String description;
+    private LocalDate date;
+    private Boolean statut;
+    private String concerner;
+
+    @ManyToOne
+    private Utilisateur utilisateur;
+}
