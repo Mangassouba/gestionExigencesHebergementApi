40f7b4e184551ef6ea2235d84bff7536068a591a
Mangassouba <hamallahmanga@gmail.com>
2025-03-24 00:32:39 +0000

    Ajout repository ressource

---
+++ src/main/java/com/gestionExigencesHebergement/gestionExigencesHebergementApi/repository/DRessourceRepository.java
@@ -0,0 +1,9 @@
+package com.gestionExigencesHebergement.gestionExigencesHebergementApi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gestionExigencesHebergement.gestionExigencesHebergementApi.model.DRessource;
+
+public interface DRessourceRepository extends JpaRepository<DRessource, Long> {
+    
+}
